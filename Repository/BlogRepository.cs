db3082e30200f98ce9091af943a8de9c7188ef68
Randy Ainsworth <randyainsworth.789@gmail.com>
2025-06-18 12:28:26 -0700

    added repository and getAllAsync method

---
+++ Repository/BlogRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using simple_blog_api_with_C_.Data;
+using simple_blog_api_with_C_.Models;
+
+namespace simple_blog_api_with_C_.Repository
+{
+    public class BlogRepository : IGenericRepository<Blog>
+    {
+        private readonly DatabaseContext _context;
+
+        public BlogRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Blog>> GetAllAsync()
+        {
+            var data = await _context.Blogs
+                .Include(b => b.Category) // Include the related Category entity
+                .ToListAsync(); // Fetch all blogs with their categories
+            return data;
+        }
+
+        public async Task<Blog?> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public async Task<Blog>? AddAsync(Blog entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Blog>? UpdateAsync(Blog entity)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public async Task<Blog>? DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+    }
+}
---
+++ Repository/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace simple_blog_api_with_C_.Repository
+{
+    public interface GenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetByIdAsync(int id);
+        Task<T>? AddAsync(T entity);
+        Task<T>? UpdateAsync(T entity);
+        Task<T>? DeleteAsync(int id);
+    }
+
+
+}
