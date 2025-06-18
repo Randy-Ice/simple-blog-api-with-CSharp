43dd189bd42e207577a78813fccf5d8ec4cf4fd3
Randy Ainsworth <randyainsworth.789@gmail.com>
2025-06-18 11:44:07 -0700

    Add project files.

---
+++ Models/Blog.cs
@@ -0,0 +1,15 @@
+﻿namespace simple_blog_api_with_C_.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public int CategoryId { get; set; }
+        public Category? Category { get; set; }
+
+
+    }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace simple_blog_api_with_C_.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
